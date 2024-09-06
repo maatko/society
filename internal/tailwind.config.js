8246b61d75415b2b9be55cf9ec9e8b9d13697bf5
maatko <matkovukovic25@gmail.com>
2024-09-06 17:03:08 +0200

    added templates directory to the generator

--- internal/tailwind.config.js
+++ internal/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  content : ['./web/template/*.templ'],
+  theme : {
+    extend : {},
   },
-  plugins: [],
+  plugins : [],
 }
-
